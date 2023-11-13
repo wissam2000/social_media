4f1b421b048d06ccb9e8fe8036e73ad847c2cd28
Wissam Mantash <42943707+wissam2000@users.noreply.github.com>
2023-11-13 16:19:01 -0500

    Finished navbar frontend but there is a bug in mobile version when accessing nav

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
+const HomePage = () => {
+    return (<Box>
+        <Navbar></Navbar>
+    </Box>)
+}
+
+export default HomePage;
